82d17fa5698fab2d9d6b632c8c777adf28a6bd20
ag1-menteconsulting <ag1@menteconsulting.com>
2026-09-22 13:14:36 +0530

    Initial commit

---
+++ primitive-datatype.java
@@ -0,0 +1,10 @@
+//doing same basic calculation with java variables
+
+class add{
+    public static void main(String[] args){
+        int a=10;
+        int b=20;
+        double c=a+b;
+        System.out.println("the sum of a and b is: "+c);
+    }
+}
